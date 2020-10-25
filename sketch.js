7ccfa0552f376e0b85d525a72d7fc757328f6444
EnriqueWiggin <70081772+EnriqueWiggin@users.noreply.github.com>
2020-10-25 19:19:25 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,34 +23,37 @@ function setup() {
   ground = new Ground();
   stand1 = new Stand(390,300,250,10);
   stand2 = new Stand(700,200,200,10);
-//level 1
-  box1 = new Box(120,275,30,40);
-  box2 = new Box(150,275,30,40);
-  box3 = new Box(180,275,30,40);
-  box4 = new Box(210,275,30,40);
-  box5 = new Box(240,275,30,40);
-  box6 = new Box(270,275,30,40);
-  box7 = new Box(300,275,30,40);
 
-  //level 2
+  box1 = new Box(300,275,30,40);
+  console.log(box1);
+  box2 = new Box(330,275,30,40);
+  box3 = new Box(360,275,30,40);
+  box4 = new Box(390,275,30,40);
+  box5 = new Box(420,275,30,40);
+  box6 = new Box(450,275,30,40);
+  box7 = new Box(480,275,30,40);
+  //level two
   box8 = new Box(330,235,30,40);
   box9 = new Box(360,235,30,40);
   box10 = new Box(390,235,30,40);
   box11 = new Box(420,235,30,40);
   box12 = new Box(450,235,30,40);
-  
-  //level 3
+  //level three
   box13 = new Box(360,195,30,40);
   box14 = new Box(390,195,30,40);
   box15 = new Box(420,195,30,40);
-
-  //Top
+  //top
   box16 = new Box(390,155,30,40);
+
 
   polygon = Bodies.circle(50,200,20);
   World.add(world,polygon);
 
  slingShot = new Slingshot(this.polygon,{x:100,y:200});
+
+ ground = new Ground();
+
+ stand1 = new Stand(390,300,250,10);
 
 }
 
@@ -74,20 +77,21 @@ function draw() {
   box14.display();
   box15.display();
   box16.display();
+  slingShot.display();
+  ground.display();
+  stand1.display();
   fill("gold");
   imageMode(CENTER)
   image(polygon_img ,polygon.position.x,polygon.position.y,40,40);
 
-  slingShot.display();
+  
 }
 function mouseDragged() {
 
-  Matter.Body.setPosition(polygon.body,{x:mouseX,y:mouseY})
+  Matter.Body.setPosition(this.polygon,{x:mouseX,y:mouseY})
 }
 function mouseReleased() {
 
-   fly(); {
-      polygon(null)
-   }
+   slingShot.fly(); 
 }
 
